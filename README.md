430393ef8283af6e6541f7686e400b768b965dec
Tim McCarthy <timothy.mcc@comcast.net>
2019-05-10 23:17:28 -0400

    tested 2nd data entry method, almost finished run loop

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # minesweeperAI
+
+a Minesweeper solver that interacts with a website that hosts minesweeper
+http://minesweeperonline.com/#
--- minesweeper.py
+++ minesweeper.py
@@ -2,13 +2,7 @@ from selenium import webdriver
 import numpy as np
 import time
 from contextlib import suppress
-from collections import Counter
-
-driver = webdriver.Chrome()
-driver.get("http://minesweeperonline.com/#intermediate")
-
-
-board = np.full((16,16),-1)
+from random import randrange
 
 
 """
@@ -17,10 +11,22 @@ in array:
 -2 = flagged
 -3 = bomb (unused?)
 0,1,2... = clicked squares with n bombs around 
-
 """
 
-#PART 1
+#PART 0 (setup)
+def setUp(difficultyStr):
+    boardSize={"beginner":(9,9), "intermediate":(16,16), "expert":(16,30)}[difficultyStr]
+    board=np.full(boardSize,-1)
+
+    driver = webdriver.Chrome()
+    
+    if difficultyStr=="expert":
+        difficultyStr=""
+    driver.get("http://minesweeperonline.com/#"+difficultyStr)
+
+    game = findGameBoard(driver)
+
+    return driver,board, game
 
 def findGameBoard(driver):
     outerContainer = driver.find_element_by_xpath("//td")
@@ -30,17 +36,19 @@ def findGameBoard(driver):
     game = gameContainer.find_element_by_id("game")
     return game
 
+#PART 1
 def addAllDataToArray(game, board):
     start=time.time()
+    
     for (i,j), item in np.ndenumerate(board):
         if item ==-1:
             board[i][j] = getDataPoint(game, i, j)
     
     print("done adding data, took " + str(time.time()-start)+" seconds")
     return board
-    #gets faster over time
 
 def getDataPoint(game, i, j):
+    #square = game.find_element_by_xpath("//div[@id='"+str(i+1)+"_"+str(j+1)+"']")
     square = game.find_element_by_id(str(i+1)+"_"+str(j+1)) #change this to speed up?
     squareData = square.get_attribute("class").split(" ")[-1][-1]
     try:
@@ -49,42 +57,85 @@ def getDataPoint(game, i, j):
         value = int(squareData)
     return value
 
-#addAllDataToArray(game,board)
-#print(game.get_attribute("id"))
+
+
+"""
+def addAllDataToArray2(game,board,initialA):
+    start=time.time()
+    a=initialA
+    i=1
+    j=1
+    while True:
+        print("checking div["+str(a)+"]")
+        square = game.find_element_by_xpath("//div["+str(a)+"]")
+        if square.get_attribute("id")==str(i)+"_"+str(j):
+            print("found "+str(i)+"_"+str(j))
+            board[i-1][j-1]=interpretSquareClass(square)
+            j+=1
+            if j==board.shape[1]+1:
+                j=1
+                i+=1
+                if i==board.shape[0]+1:
+                    break
+        a+=1
+                
+
+    print("done adding data, took " + str(time.time()-start)+" seconds")
+    return board
+
+def interpretSquareClass(square):
+    squareData = square.get_attribute("class").split(" ")[-1][-1]
+    try:
+        value = {"d":-2, "k":-1}[squareData] #flagge"d" -> -2, blan"k" -> -1
+    except KeyError:
+        value = int(squareData)
+    return value
+    
+
+def find1_1Div(game):
+    a=1
+    while True:
+        possibleSquare = game.find_element_by_xpath("//div["+str(a)+"]")
+        if possibleSquare.get_attribute("id")=="1_1":
+            break
+        else:
+            a+=1
+    return a
+"""
+
 
 #PART 2
-#takes in array
-#will return a list of i,j pairs to be flagged and a list to be clicked
 def flagOrClick(board):
+    start=time.time()
     flagSquareList=[]
     emptySquareList=[]
     
     for (i,j), item in np.ndenumerate(board):
         if item>0:
-            neighborList = getNeighbors(board,i,j)
-            #print(i, j, board[i][j], neighborList)
+            neighborList = getBoardNeighbors(board,i,j)
+            
             d={-2:0, -1:0}
             for a,b,board[a][b] in neighborList:
                 d[board[a][b]]=d.get(board[a][b],0)+1
-            #print(d)
-            #print(str(d[-2])+"=?="+str(item))
+                
+            if d[-1]+d[-2]==item:
+                for x in neighborList:
+                    if x[2]==-1:
+                        flagSquareList.append(x)    
+
+            d={-2:0, -1:0}
+            for a,b,board[a][b] in neighborList:
+                d[board[a][b]]=d.get(board[a][b],0)+1
+                
             if d[-2]==item:
                 for x in neighborList:
                     if x[2]==-1:
-                        #print("added" + str(x) + "to left click")
                         emptySquareList.append(x)
-            else:
-                #print(str(d[-1]+d[-2]) + "=??=" + str(item))
-                if d[-1]+d[-2]==item:
-                    #print("sum found")
-                    for x in neighborList:
-                        if x[2]==-1:
-                            #print("added" + str(x) + "to right click")
-                            flagSquareList.append(x)
-            #print("")
+            
+    print("done deciding clicks, took " + str(time.time()-start)+" seconds")                    
     return list(set(emptySquareList)), list(set(flagSquareList))
         
-def getNeighbors(board,i,j):
+def getBoardNeighbors(board,i,j):
     neighborList=[]
     if i!=0 and j!=0:
         neighborList.append((i-1,j-1,board[i-1][j-1]))
@@ -103,12 +154,15 @@ def getNeighbors(board,i,j):
             neighborList.append((i+1,j-1,board[i+1][j-1]))
         neighborList.append((i,j-1,board[i][j-1]))
     return neighborList
-        
+
+def flagOrClickWithNoMovesLeft(board, leftClickList):
+    i,j = np.where(board==-1)
+    rand=randrange(len(i))
+    leftClickList.append((i[rand],j[rand]))
+    #print(i,j)
 
 
 #PART 3
-#takes in the 2 lists, clicks as such
-
 def clickAll(game,leftClickList, rightClickList):
     actions = webdriver.common.action_chains.ActionChains(driver)
     for pair in leftClickList:
@@ -124,36 +178,28 @@ def clickOnElt(actions,game,i,j,left):
         actions.click(squareToClick)
     else:
         actions.context_click(squareToClick)
+        board[i][j]=-2
 
-
-game = findGameBoard(driver)
-clickAll(game,[(8,8)],[])
-#board1 = addAllDataToArray(game,board)
-#print(board1)
-#list1,list2=flagOrClick(board1)
-#clickAll(game,list1,list2)
-
-while True:
-    board1 = addAllDataToArray(game,board)
-    if np.all(board1!=-1):
-        break
-    #print(board1)
-    list1,list2=flagOrClick(board1)
-    clickAll(game,list1,list2)
+#RUN
+if __name__ == "__main__":
+    driver, board, game = setUp("beginner")
     
+    clickAll(game,[(randrange(board.shape[0]+1),randrange(board.shape[1]+1))],[])
     
-"""
-1 - get data from website -> array
-    - data is stored in HTML
-    - navigate thru HTML - get id + class 
+    while True:
+        board = addAllDataToArray(game,board)
+        list1, list2 = flagOrClick(board)
+        if len(list1)==0 and len(list2)==0:
+            flagOrClickWithNoMovesLeft(board, list1)
+            print("no moves found, picking random")
+        clickAll(game,list1,list2)
+        try:
+            alert = driver.switch_to.alert
+            print("game over!")
+            break
+        except: #NoAlertPresentException
+            pass
+    
 
-2 - do things with data -> square to click on
 
-
-3 - click on square in website -> new data on site
-
-repeat
-
-
-"""
-#driver.quit()
+    
