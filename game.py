6d645ddfc4ed58df8d5848620a50bb09b96c1fcf
Tim McCarthy <timothy.mcc@comcast.net>
2019-06-15 10:12:46 -0400

    formatting

--- game.py
+++ game.py
@@ -3,13 +3,14 @@ from board import BoardData
 import time
 from contextlib import suppress
 
+
 class Game:
-    
+
     def __init__(self, driver, difficultyStr):
         boardObj = BoardData(difficultyStr)
-        
-        if difficultyStr=="expert":
-            difficultyStr=""
+
+        if difficultyStr == "expert":
+            difficultyStr = ""
         driver.get("http://minesweeperonline.com/#"+difficultyStr)
         game = self.findGameBoard(driver)
 
@@ -19,62 +20,63 @@ class Game:
         self.game = game
         self.boardObj = boardObj
         self.loss = loss
-        
+
     def findGameBoard(self, driver):
         outerContainer = driver.find_element_by_xpath("//td")
-        innerContainer = outerContainer.find_element_by_class_name("inner-container")
+        innerContainer = outerContainer.find_element_by_class_name(
+            "inner-container")
         centerColumn = innerContainer.find_element_by_id("center-column")
         gameContainer = centerColumn.find_element_by_id("game-container")
         game = gameContainer.find_element_by_id("game")
-        
+
         return game
 
-
-    
     def addNewDataToArray(self, leftClickList):
-        start=time.time()
-        checkedList=[]
+        start = time.time()
+        checkedList = []
         for x in leftClickList:
-            self.updateArrayWithNeighbors(x[0],x[1],checkedList)
-        print("done adding data recursively, took " + str(time.time()-start)+" seconds")
+            self.updateArrayWithNeighbors(x[0], x[1], checkedList)
+        print("done adding data recursively, took " +
+              str(time.time()-start)+" seconds")
 
     def updateArrayWithNeighbors(self, i, j, checkedList):
 
-        checkedList.append((i,j))
-        
-        value=self.getDataPoint(i,j)
-        self.boardObj.boardArray[i][j]=value
+        checkedList.append((i, j))
 
-        if value==0:
-            if i!=0 and j!=0 and (i-1,j-1) not in checkedList:
-                self.updateArrayWithNeighbors(i-1,j-1,checkedList)
-            if i!=0:
-                if (i-1,j) not in checkedList:
-                    self.updateArrayWithNeighbors(i-1,j,checkedList)
+        value = self.getDataPoint(i, j)
+        self.boardObj.boardArray[i][j] = value
+
+        if value == 0:
+            if i != 0 and j != 0 and (i-1, j-1) not in checkedList:
+                self.updateArrayWithNeighbors(i-1, j-1, checkedList)
+            if i != 0:
+                if (i-1, j) not in checkedList:
+                    self.updateArrayWithNeighbors(i-1, j, checkedList)
                 with suppress(IndexError):
-                    if (i-1,j+1) not in checkedList:
-                        self.updateArrayWithNeighbors(i-1,j+1,checkedList)
+                    if (i-1, j+1) not in checkedList:
+                        self.updateArrayWithNeighbors(i-1, j+1, checkedList)
             with suppress(IndexError):
-                if (i,j+1) not in checkedList:
-                    self.updateArrayWithNeighbors(i,j+1,checkedList)
+                if (i, j+1) not in checkedList:
+                    self.updateArrayWithNeighbors(i, j+1, checkedList)
             with suppress(IndexError):
-                if (i+1,j+1) not in checkedList:
-                    self.updateArrayWithNeighbors(i+1,j+1,checkedList)
+                if (i+1, j+1) not in checkedList:
+                    self.updateArrayWithNeighbors(i+1, j+1, checkedList)
             with suppress(IndexError):
-                if (i+1,j) not in checkedList:
-                    self.updateArrayWithNeighbors(i+1,j,checkedList)
-            if j!=0:
+                if (i+1, j) not in checkedList:
+                    self.updateArrayWithNeighbors(i+1, j, checkedList)
+            if j != 0:
                 with suppress(IndexError):
-                    if (i+1,j-1) not in checkedList:
-                        self.updateArrayWithNeighbors(i+1,j-1,checkedList)
-                if (i,j-1) not in checkedList:
-                    self.updateArrayWithNeighbors(i,j-1,checkedList)
+                    if (i+1, j-1) not in checkedList:
+                        self.updateArrayWithNeighbors(i+1, j-1, checkedList)
+                if (i, j-1) not in checkedList:
+                    self.updateArrayWithNeighbors(i, j-1, checkedList)
 
     def getDataPoint(self, i, j):
         square = self.game.find_element_by_id(str(i+1)+"_"+str(j+1))
         squareData = square.get_attribute("class").split(" ")[-1][-1]
         try:
-            value = {"d":-2, "k":-1}[squareData] #flagge"d" -> -2, blan"k" -> -1
+            # flagge"d" -> -2, blan"k" -> -1
+            value = {"d": -2, "k": -1}[squareData]
         except KeyError:
             try:
                 value = int(squareData)
@@ -83,28 +85,22 @@ class Game:
                 self.loss = True
         return value
 
-
-    
     def clickAll(self, leftClickList, rightClickList):
         actions = webdriver.common.action_chains.ActionChains(self.driver)
         for pair in leftClickList:
-            self.clickOnElt(actions,pair[0],pair[1],True)
+            self.clickOnElt(actions, pair[0], pair[1], True)
         for pair in rightClickList:
-            if self.boardObj.boardArray[pair[0]][pair[1]]==-1:
-                self.clickOnElt(actions,pair[0],pair[1],False)
+            if self.boardObj.boardArray[pair[0]][pair[1]] == -1:
+                self.clickOnElt(actions, pair[0], pair[1], False)
         try:
             actions.perform()
-        except: #alert error if halfway through clicking
+        except:  # alert error if halfway through clicking
             pass
 
-    def clickOnElt(self, actions,i,j,left):
+    def clickOnElt(self, actions, i, j, left):
         squareToClick = self.game.find_element_by_id(str(i+1)+"_"+str(j+1))
         if left:
             actions.click(squareToClick)
         else:
             actions.context_click(squareToClick)
-            self.boardObj.boardArray[i][j]=-2
-
-
-
-    
+            self.boardObj.boardArray[i][j] = -2
--- run.py
+++ run.py
@@ -6,14 +6,15 @@ import selenium
 def Main(difficultyStr, losses, driver):
     gameObj = Game(driver, difficultyStr)
 
-    listL=[(randrange(gameObj.boardObj.boardArray.shape[0]),randrange(gameObj.boardObj.boardArray.shape[1]))]
-    gameObj.clickAll(listL,[])
+    listL = [(randrange(gameObj.boardObj.boardArray.shape[0]),
+              randrange(gameObj.boardObj.boardArray.shape[1]))]
+    gameObj.clickAll(listL, [])
 
     while not gameObj.loss:
         gameObj.addNewDataToArray(listL)
 
         listL, listR = gameObj.boardObj.flagAndClick()
-        if len(listL)==0 and len(listR)==0:
+        if len(listL) == 0 and len(listR) == 0:
             gameObj.boardObj.flagOrClickWithNoMovesLeft(listL)
             print("no moves found, picking random")
         gameObj.clickAll(listL, listR)
@@ -21,20 +22,23 @@ def Main(difficultyStr, losses, driver):
             alert = gameObj.driver.switch_to.alert
             print("you won after "+str(losses)+" losses")
             break
-        except selenium.common.exceptions.NoAlertPresentException: #NoAlertPresentException
+        except selenium.common.exceptions.NoAlertPresentException:  # NoAlertPresentException
             pass
     if gameObj.loss:
-        face=gameObj.game.find_element_by_id("face")
-        action = selenium.webdriver.common.action_chains.ActionChains(gameObj.driver)
+        face = gameObj.game.find_element_by_id("face")
+        action = selenium.webdriver.common.action_chains.ActionChains(
+            gameObj.driver)
         action.click(face)
         action.perform()
-        gameObj.loss=False
-        print("you've lost "+str(losses)+"x")
-        losses+=1
+        gameObj.loss = False
+        print("you've lost "+str(losses+1)+"x")
+        losses += 1
         Main(difficultyStr, losses, driver)
 
-if __name__ == "__main__":
-    losses=0
+
+def playTilWin():
+    losses = 0
     driver = selenium.webdriver.Chrome()
     Main("expert", losses, driver)
+    driver.quit()
     return losses
